02c0064cf0b352aa253bc84b19e2443cf1c48793
chengzeng <czen0002@student.monash.edu>
2020-02-24 09:10:08 +1100

    generate four components: home, setting, login and track

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AdminComponent } from './admin/admin.component';
+import { LoginComponent } from './login/login.component';
+import { TrackComponent } from './track/track.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AdminComponent,
+    LoginComponent,
+    TrackComponent
   ],
   imports: [
     BrowserModule,
