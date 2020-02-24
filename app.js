77e62a46436039885a38318d38b055c2b9d308c4
chengzeng <czen0002@student.monash.edu>
2020-02-24 14:57:44 +1100

    create api for getting and posting color in app.js

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongodb = require('mongodb');
+const fs = require('fs');
 
 const mongoURI = "mongodb://" + process.argv[2] + ":27017/infosys";
 console.log("Connecting to MongoDB Service=" + mongoURI);
 let path = require('path');
+
 
 //const records = require('./routers/record');
 
@@ -33,3 +35,46 @@ mongodb.MongoClient.connect(mongoURI, function(err, client) {
     console.log("Web App is running on port 4200");
   });
 });
+
+
+// Generic error handler
+function handleError(res, reason, message, code) {
+    console.log("ERROR: " + reason);
+    res.status(code || 500).json({'error': message});
+}
+
+//RESTful API for setting(backgroud color, input fields, logo imgae)
+// read color and upload color
+app.get('/color', function (req, res){
+    fs.readFile('dist/admin/assets/color/color.json', function(err, content) {
+        if (err) {
+            handleError(res, err.message, "Failed to get color.");
+        } else {
+            res.status(200).json(content);
+        }
+    })
+});
+  
+app.post('/color', function (req, res){
+    const jsonString = JSON.stringify(req.body);
+    if (!jsonString) {
+        handleError(res, "Invalid color JSON file", "Must provide a color.", 400);
+    } else {
+        fs.writeFile('dist/admin/assets/color/color.json', jsonString, function(err, content){
+            if (err) {
+                handleError(res, err.message, "Failed to write color into dist/admin/assets.");
+            } else {
+                res.status(201).json(content);
+            }
+        })
+    
+        fs.writeFile('src/assets/color/color.json', jsonString, function(err, content){
+            if (err) {
+                handleError(res, err.message, "Failed to write color into src/assets.");
+            } else {
+                res.status(201).json(content);
+            }
+        })
+    }
+    
+})
